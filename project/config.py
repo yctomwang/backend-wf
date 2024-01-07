d60af9d702cb83f9fe6c97adc51a019dfda46f5e
Thomas W <69774548+yctomwang@users.noreply.github.com>
2024-01-07 16:45:31 +1100

    Update config.py
    update the config files to suit the needs of docker compose file

--- project/config.py
+++ project/config.py
@@ -8,14 +8,17 @@ class BaseConfig:
     BASE_DIR = Path(__file__).parent.parent
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = os.environ.get(
+    #     "DATABASE_URL", "postgresql://test_pam:test_pam@127.0.0.1:5432/test_pam"
+    # )
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL", "postgresql://test_pam:test_pam@127.0.0.1:5432/test_pam"
+    "DATABASE_URL", "postgresql://test_pam:test_pam@database:5432/test_pam"
     )
     CELERY_BROKER_URL = os.environ.get(
-        "CELERY_BROKER_URL", "pyamqp://guest:guest@localhost//"
+        "CELERY_BROKER_URL", "pyamqp://guest:guest@rabbitmq//"
     )  # new
     CELERY_RESULT_BACKEND = os.environ.get(
-        "CELERY_RESULT_BACKEND", "postgresql://test_pam:test_pam@127.0.0.1:5432/test_pam"
+        "CELERY_RESULT_BACKEND", "postgresql://test_pam:test_pam@database:5432/test_pam"
     )  # new
 
 
