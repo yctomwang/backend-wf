57cbb0343219dac265c820c8f430e53fca0fdd53
Thomas W <69774548+yctomwang@users.noreply.github.com>
2024-01-07 21:23:47 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Wait for the database to be ready
-echo "Waiting for database to be ready..."
-while ! nc -z database 5432; do
-  sleep 0.1
-done
-echo "Database is ready!"
 
 # Run DB Migrations
 flask db upgrade
