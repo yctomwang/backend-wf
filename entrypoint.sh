ba1681142064121b2881f480b88869b0b1d25d0a
Thomas W <69774548+yctomwang@users.noreply.github.com>
2024-01-07 20:45:19 +1100

    Update entrypoint.sh
    add a while ready to see if it solves the problem.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# Wait for the database to be ready
+echo "Waiting for database to be ready..."
+while ! nc -z database 5432; do
+  sleep 0.1
+done
+echo "Database is ready!"
+
+# Run DB Migrations
 flask db upgrade
+
+# Start the application
 gunicorn app:app -w 2 --threads 2 -b 0.0.0.0:5000 --reload
+
